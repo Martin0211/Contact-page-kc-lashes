df1ebf1ba4b00d9ab9a80d9d698f2e503eba1b55
Martín0211 <martin021191@gmail.com>
2023-11-18 14:53:40 -0600

    frontend

---
+++ src/components/ButtonShare.jsx
@@ -0,0 +1,9 @@
+import { BiShareAlt } from 'react-icons/bi';
+
+export default function ButtonShare() {
+    return(
+        <button className='bg-slate-200 text-black rounded-full h-8 px-3'>
+            <BiShareAlt/>
+        </button>
+    )
+}
