90546a483dcf4b0ce4eb4adf8f9c5c604d5072ed
Martín0211 <martin021191@gmail.com>
2024-01-16 12:09:54 -0500

    se agregaron estilos al Button

--- src/components/Ubication.jsx
+++ src/components/Ubication.jsx
@@ -2,8 +2,8 @@ import { FaLocationDot } from 'react-icons/fa6';
 
 export default function ButtonLocation() {
     return(
-        <button className='flex items-center mb-4'>
-            <FaLocationDot className='m-1'/> Queretaro
+        <button className='flex items-center mb-4 text-white'>
+            <FaLocationDot className='m-1 text-white' /> Queretaro
         </button>
     )
 }
